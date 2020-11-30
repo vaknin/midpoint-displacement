fa552b11a2b6c354b112c05157a1a78d703725cd
Aviv <vaknins33@gmail.com>
2020-11-30 22:44:34 +0200

    space progress

--- script.ts
+++ script.ts
@@ -18,12 +18,51 @@ const rand = (min:number, max:number) => {
     return Math.random() * (max-min) + min
 }
 
-const draw_background = () => {
+const draw_background = (color:string) => {
     ctx.beginPath();
     ctx.rect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = color;
     ctx.fill();
     ctx.closePath();
+}
+
+const draw_space = () => {
+
+    const draw_stars = (amount, size, color="#fff") => {
+
+        for (let i = 0; i < amount; i++) {
+            const radius = rand(0.01, size);
+            const pos_x = Math.random() * (canvas.width - radius) + radius;
+            const pos_y = Math.random() * (canvas.height - radius) + radius;
+            ctx.beginPath();
+            ctx.arc(pos_x, pos_y, radius, 0, Math.PI*2);
+            ctx.fillStyle = color;
+            ctx.fill();
+        }
+    }
+
+    const draw_trail = () => {
+        const start_point = new Point(rand(0, canvas.width), rand(0, canvas.height-mountains_seed));
+        let angle = rand(0, 2*Math.PI);
+        while ((angle > Math.PI / 4 && angle < 3*Math.PI/4) ||(angle > 5*Math.PI/4 && angle < 7*Math.PI/4)) {
+            angle = rand(0, 2*Math.PI);
+        }
+        const length = rand(150, 250);
+        const end_point = new Point(start_point.x + (Math.cos(angle) * length), start_point.y + (Math.sin(angle) * length))
+
+        ctx.beginPath();
+        ctx.moveTo(start_point.x, start_point.y);
+        ctx.lineTo(end_point.x, end_point.y)
+        ctx.closePath();
+        ctx.strokeStyle = '#bbb';
+        ctx.lineWidth = .1;
+        ctx.stroke();
+    }
+
+    draw_stars(7000, 0.3, '#fff')// Small stars
+    draw_stars(3000, 1, '#fff')// Big stars
+    draw_stars(1000, 0.3, 'magenta')// Small purple stars
+    draw_trail()
 }
 
 const calculate_distance = (p1:Point, p2:Point) => {
@@ -44,7 +83,7 @@ const displace_midpoint = (points:Point[], random_coefficient:number) => {
         const new_point = new Point(x_midpoint, new_point_y);
         new_points.splice(i+i-1, 0, new_point);
     }
-    random_coefficient *= 0.52;
+    random_coefficient *= rand(0.35, 0.6);
 
     return {
         points: new_points,
@@ -52,44 +91,55 @@ const displace_midpoint = (points:Point[], random_coefficient:number) => {
     }
 }
 
-const ceiling = 0;
+const ceiling = 50;
 const floor = 0;
-const point_2 = new Point(canvas.width, rand(canvas.height, floor));
+const displacements = 12;
+const mountains_seed = 300;
+const point_2 = new Point(canvas.width, rand(canvas.height, canvas.height));
 const point_1 = new Point(0, Math.max(rand(canvas.height, floor), point_2.y));
-let random_coefficient = canvas.height;
 
-const draw = () => {
+const draw_mountains = (amount:number, random_coefficient:number) => {
 
-    draw_background()
-
-    let points = [point_1, point_2];
-
-
-    // Displace
-    for (let i = 0; i < 15; i++) {
-        const res = displace_midpoint(points, random_coefficient);
-        points = res.points;
-        random_coefficient = res.coefficient;
+    const original_random_coefficient = random_coefficient;
+    
+    for (let i = 0; i < amount; i++) {
+        
+        let points = [point_1, point_2];
+        random_coefficient = original_random_coefficient * rand(0.05, 2.5);
+    
+        // Displace
+        for (let i = 0; i < 12; i++) {
+            const res = displace_midpoint(points, random_coefficient);
+            points = res.points;
+            random_coefficient = res.coefficient;
+        }
+    
+        // Predraw
+        ctx.beginPath();
+        ctx.strokeStyle = "#000";
+        const color0 = rand(31, 35);
+        const color1 = rand(31, 35);
+        const color2 = rand(31, 35);
+        ctx.fillStyle = `rgb(${color0}, ${color1}, ${color2})`;
+        ctx.lineWidth = 0.5;
+    
+    
+        // Make lines
+        ctx.moveTo(points[0].x, points[0].y)
+        for (let i = 1; i < points.length; i++) {
+            ctx.lineTo(points[i].x, points[i].y)
+        }
+    
+        // Stroke
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill()
     }
-    console.log(random_coefficient)
-
-    // Predraw
-    ctx.beginPath();
-    ctx.strokeStyle = "#fff"; // Important
-    ctx.lineWidth = 1; // Important
-
-    // Make lines
-    ctx.moveTo(points[0].x, points[0].y)
-    for (let i = 1; i < points.length; i++) {
-        ctx.lineTo(points[i].x, points[i].y)
-    }
-
-    // Stroke
-    ctx.stroke();
-    ctx.closePath();
 }
 
-draw()
+draw_background('#000')
+draw_space()
+draw_mountains(100, mountains_seed);
 
 // const loop = () => {
 //     draw_background()
--- typescript-compiling/script.js
+++ typescript-compiling/script.js
@@ -14,12 +14,46 @@ var Point = /** @class */ (function () {
 var rand = function (min, max) {
     return Math.random() * (max - min) + min;
 };
-var draw_background = function () {
+var draw_background = function (color) {
     ctx.beginPath();
     ctx.rect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#000";
+    ctx.fillStyle = color;
     ctx.fill();
     ctx.closePath();
+};
+var draw_space = function () {
+    var draw_stars = function (amount, size, color) {
+        if (color === void 0) { color = "#fff"; }
+        for (var i = 0; i < amount; i++) {
+            var radius = rand(0.01, size);
+            var pos_x = Math.random() * (canvas.width - radius) + radius;
+            var pos_y = Math.random() * (canvas.height - radius) + radius;
+            ctx.beginPath();
+            ctx.arc(pos_x, pos_y, radius, 0, Math.PI * 2);
+            ctx.fillStyle = color;
+            ctx.fill();
+        }
+    };
+    var draw_trail = function () {
+        var start_point = new Point(rand(0, canvas.width), rand(0, canvas.height - mountains_seed));
+        var angle = rand(0, 2 * Math.PI);
+        while ((angle > Math.PI / 4 && angle < 3 * Math.PI / 4) || (angle > 5 * Math.PI / 4 && angle < 7 * Math.PI / 4)) {
+            angle = rand(0, 2 * Math.PI);
+        }
+        var length = rand(150, 250);
+        var end_point = new Point(start_point.x + (Math.cos(angle) * length), start_point.y + (Math.sin(angle) * length));
+        ctx.beginPath();
+        ctx.moveTo(start_point.x, start_point.y);
+        ctx.lineTo(end_point.x, end_point.y);
+        ctx.closePath();
+        ctx.strokeStyle = '#bbb';
+        ctx.lineWidth = .1;
+        ctx.stroke();
+    };
+    draw_stars(7000, 0.3, '#fff'); // Small stars
+    draw_stars(3000, 1, '#fff'); // Big stars
+    draw_stars(1000, 0.3, 'magenta'); // Small purple stars
+    draw_trail();
 };
 var calculate_distance = function (p1, p2) {
     return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
@@ -35,41 +69,51 @@ var displace_midpoint = function (points, random_coefficient) {
         var new_point = new Point(x_midpoint, new_point_y);
         new_points.splice(i + i - 1, 0, new_point);
     }
-    random_coefficient *= 0.52;
+    random_coefficient *= rand(0.35, 0.6);
     return {
         points: new_points,
         coefficient: random_coefficient
     };
 };
-var ceiling = 0;
+var ceiling = 50;
 var floor = 0;
-var point_2 = new Point(canvas.width, rand(canvas.height, floor));
+var displacements = 12;
+var mountains_seed = 300;
+var point_2 = new Point(canvas.width, rand(canvas.height, canvas.height));
 var point_1 = new Point(0, Math.max(rand(canvas.height, floor), point_2.y));
-var random_coefficient = canvas.height;
-var draw = function () {
-    draw_background();
-    var points = [point_1, point_2];
-    // Displace
-    for (var i = 0; i < 15; i++) {
-        var res = displace_midpoint(points, random_coefficient);
-        points = res.points;
-        random_coefficient = res.coefficient;
+var draw_mountains = function (amount, random_coefficient) {
+    var original_random_coefficient = random_coefficient;
+    for (var i = 0; i < amount; i++) {
+        var points = [point_1, point_2];
+        random_coefficient = original_random_coefficient * rand(0.05, 2.5);
+        // Displace
+        for (var i_1 = 0; i_1 < 12; i_1++) {
+            var res = displace_midpoint(points, random_coefficient);
+            points = res.points;
+            random_coefficient = res.coefficient;
+        }
+        // Predraw
+        ctx.beginPath();
+        ctx.strokeStyle = "#000";
+        var color0 = rand(31, 35);
+        var color1 = rand(31, 35);
+        var color2 = rand(31, 35);
+        ctx.fillStyle = "rgb(" + color0 + ", " + color1 + ", " + color2 + ")";
+        ctx.lineWidth = 0.5;
+        // Make lines
+        ctx.moveTo(points[0].x, points[0].y);
+        for (var i_2 = 1; i_2 < points.length; i_2++) {
+            ctx.lineTo(points[i_2].x, points[i_2].y);
+        }
+        // Stroke
+        ctx.closePath();
+        ctx.stroke();
+        ctx.fill();
     }
-    console.log(random_coefficient);
-    // Predraw
-    ctx.beginPath();
-    ctx.strokeStyle = "#fff"; // Important
-    ctx.lineWidth = 1; // Important
-    // Make lines
-    ctx.moveTo(points[0].x, points[0].y);
-    for (var i = 1; i < points.length; i++) {
-        ctx.lineTo(points[i].x, points[i].y);
-    }
-    // Stroke
-    ctx.stroke();
-    ctx.closePath();
 };
-draw();
+draw_background('#000');
+draw_space();
+draw_mountains(100, mountains_seed);
 // const loop = () => {
 //     draw_background()
 //     window.requestAnimationFrame(loop)
