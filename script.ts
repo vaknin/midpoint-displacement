ec07e3ca566ea106153049b0088ec5b8dd958951
Aviv <vaknins33@gmail.com>
2020-12-01 12:16:38 +0200

    added download, some number tweaks

--- script.ts
+++ script.ts
@@ -1,7 +1,7 @@
 //#region Canvas Initialization
 const canvas:HTMLCanvasElement = document.querySelector('#canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 3840//window.innerWidth;
+canvas.height = 1080//window.innerHeight;
 const ctx = canvas.getContext('2d');
 //#endregion
 
@@ -42,7 +42,8 @@ const draw_space = () => {
     }
 
     const draw_trail = () => {
-        const start_point = new Point(rand(0, canvas.width), rand(0, canvas.height-mountains_seed));
+        const start_point = new Point(rand(0, canvas.width), rand(0, canvas.height-mountains_height_seed));
+        // Get a proper random angle
         let angle = rand(0, 2*Math.PI);
         while ((angle > Math.PI / 4 && angle < 3*Math.PI/4) ||(angle > 5*Math.PI/4 && angle < 7*Math.PI/4)) {
             angle = rand(0, 2*Math.PI);
@@ -59,10 +60,12 @@ const draw_space = () => {
         ctx.stroke();
     }
 
-    draw_stars(7000, 0.3, '#fff')// Small stars
-    draw_stars(3000, 1, '#fff')// Big stars
-    draw_stars(1000, 0.3, 'magenta')// Small purple stars
-    draw_trail()
+    draw_stars(canvas.width*4, 0.3, '#fff')// Small stars
+    draw_stars(canvas.width*2, 1, '#fff')// Big stars
+    draw_stars(canvas.width/2, 0.3, 'magenta')// Small purple stars
+    for (let i = 0; i < Math.floor(canvas.width/1680); i++) {
+        draw_trail()
+    }
 }
 
 const calculate_distance = (p1:Point, p2:Point) => {
@@ -93,36 +96,41 @@ const displace_midpoint = (points:Point[], random_coefficient:number) => {
 
 const ceiling = 50;
 const floor = 0;
-const displacements = 12;
-const mountains_seed = 300;
+const displacements = 10;
+const mountains_height_seed = canvas.height * 0.7;
+const mountains_width_seed = 0.9;
+const num_of_mountains = 20;
 const point_2 = new Point(canvas.width, rand(canvas.height, canvas.height));
 const point_1 = new Point(0, Math.max(rand(canvas.height, floor), point_2.y));
 
 const draw_mountains = (amount:number, random_coefficient:number) => {
 
-    const original_random_coefficient = random_coefficient;
+    ctx.lineWidth = mountains_width_seed;
+    let original_random_coefficient = random_coefficient;
     
     for (let i = 0; i < amount; i++) {
         
         let points = [point_1, point_2];
-        random_coefficient = original_random_coefficient * rand(0.05, 2.5);
+        random_coefficient = original_random_coefficient * rand(0.6, 1.4);
     
         // Displace
-        for (let i = 0; i < 12; i++) {
+        for (let i = 0; i < displacements; i++) {
             const res = displace_midpoint(points, random_coefficient);
             points = res.points;
             random_coefficient = res.coefficient;
         }
+
+        original_random_coefficient *= 0.9;
     
         // Predraw
         ctx.beginPath();
-        ctx.strokeStyle = "#000";
-        const color0 = rand(31, 35);
-        const color1 = rand(31, 35);
-        const color2 = rand(31, 35);
+        ctx.strokeStyle = "#555";
+        const color0 = rand(0, 100);
+        const color1 = rand(52, 63);
+        const color2 = rand(52, 63);
         ctx.fillStyle = `rgb(${color0}, ${color1}, ${color2})`;
-        ctx.lineWidth = 0.5;
-    
+        ctx.lineWidth *= 1.1;
+        ctx.lineWidth = Math.min(2.5, ctx.lineWidth);
     
         // Make lines
         ctx.moveTo(points[0].x, points[0].y)
@@ -137,14 +145,14 @@ const draw_mountains = (amount:number, random_coefficient:number) => {
     }
 }
 
-draw_background('#000')
+draw_background(`rgb(${rand(25, 40)},0,${rand(30, 45)})`)
 draw_space()
-draw_mountains(100, mountains_seed);
+draw_mountains(num_of_mountains, mountains_height_seed);
 
-// const loop = () => {
-//     draw_background()
-
-//     window.requestAnimationFrame(loop)
-// }
-
-// window.requestAnimationFrame(loop)
+// Download
+// const url = canvas.toDataURL("image/png");
+// const a = document.createElement('a');
+// document.body.appendChild(a);
+// a.download = 'image';
+// a.href = url;
+// a.click();
--- typescript-compiling/script.js
+++ typescript-compiling/script.js
@@ -1,7 +1,7 @@
 //#region Canvas Initialization
 var canvas = document.querySelector('#canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 3840; //window.innerWidth;
+canvas.height = 1080; //window.innerHeight;
 var ctx = canvas.getContext('2d');
 //#endregion
 var Point = /** @class */ (function () {
@@ -35,7 +35,8 @@ var draw_space = function () {
         }
     };
     var draw_trail = function () {
-        var start_point = new Point(rand(0, canvas.width), rand(0, canvas.height - mountains_seed));
+        var start_point = new Point(rand(0, canvas.width), rand(0, canvas.height - mountains_height_seed));
+        // Get a proper random angle
         var angle = rand(0, 2 * Math.PI);
         while ((angle > Math.PI / 4 && angle < 3 * Math.PI / 4) || (angle > 5 * Math.PI / 4 && angle < 7 * Math.PI / 4)) {
             angle = rand(0, 2 * Math.PI);
@@ -50,10 +51,12 @@ var draw_space = function () {
         ctx.lineWidth = .1;
         ctx.stroke();
     };
-    draw_stars(7000, 0.3, '#fff'); // Small stars
-    draw_stars(3000, 1, '#fff'); // Big stars
-    draw_stars(1000, 0.3, 'magenta'); // Small purple stars
-    draw_trail();
+    draw_stars(canvas.width * 4, 0.3, '#fff'); // Small stars
+    draw_stars(canvas.width * 2, 1, '#fff'); // Big stars
+    draw_stars(canvas.width / 2, 0.3, 'magenta'); // Small purple stars
+    for (var i = 0; i < Math.floor(canvas.width / 1680); i++) {
+        draw_trail();
+    }
 };
 var calculate_distance = function (p1, p2) {
     return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
@@ -77,29 +80,34 @@ var displace_midpoint = function (points, random_coefficient) {
 };
 var ceiling = 50;
 var floor = 0;
-var displacements = 12;
-var mountains_seed = 300;
+var displacements = 10;
+var mountains_height_seed = canvas.height * 0.7;
+var mountains_width_seed = 0.9;
+var num_of_mountains = 20;
 var point_2 = new Point(canvas.width, rand(canvas.height, canvas.height));
 var point_1 = new Point(0, Math.max(rand(canvas.height, floor), point_2.y));
 var draw_mountains = function (amount, random_coefficient) {
+    ctx.lineWidth = mountains_width_seed;
     var original_random_coefficient = random_coefficient;
     for (var i = 0; i < amount; i++) {
         var points = [point_1, point_2];
-        random_coefficient = original_random_coefficient * rand(0.05, 2.5);
+        random_coefficient = original_random_coefficient * rand(0.6, 1.4);
         // Displace
-        for (var i_1 = 0; i_1 < 12; i_1++) {
+        for (var i_1 = 0; i_1 < displacements; i_1++) {
             var res = displace_midpoint(points, random_coefficient);
             points = res.points;
             random_coefficient = res.coefficient;
         }
+        original_random_coefficient *= 0.9;
         // Predraw
         ctx.beginPath();
-        ctx.strokeStyle = "#000";
-        var color0 = rand(31, 35);
-        var color1 = rand(31, 35);
-        var color2 = rand(31, 35);
+        ctx.strokeStyle = "#555";
+        var color0 = rand(0, 100);
+        var color1 = rand(52, 63);
+        var color2 = rand(52, 63);
         ctx.fillStyle = "rgb(" + color0 + ", " + color1 + ", " + color2 + ")";
-        ctx.lineWidth = 0.5;
+        ctx.lineWidth *= 1.1;
+        ctx.lineWidth = Math.min(2.5, ctx.lineWidth);
         // Make lines
         ctx.moveTo(points[0].x, points[0].y);
         for (var i_2 = 1; i_2 < points.length; i_2++) {
@@ -111,11 +119,13 @@ var draw_mountains = function (amount, random_coefficient) {
         ctx.fill();
     }
 };
-draw_background('#000');
+draw_background("rgb(" + rand(25, 40) + ",0," + rand(30, 45) + ")");
 draw_space();
-draw_mountains(100, mountains_seed);
-// const loop = () => {
-//     draw_background()
-//     window.requestAnimationFrame(loop)
-// }
-// window.requestAnimationFrame(loop)
+draw_mountains(num_of_mountains, mountains_height_seed);
+// Download
+// const url = canvas.toDataURL("image/png");
+// const a = document.createElement('a');
+// document.body.appendChild(a);
+// a.download = 'image';
+// a.href = url;
+// a.click();
